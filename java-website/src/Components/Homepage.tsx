a22aaad28578989aa9b99177b8cf03d2e797aae5
TFM-HITESH <hiteshshivkumar@gmail.com>
2023-08-27 19:35:45 +0530

    Background Image

---
+++ java-website/src/Components/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type HomepageProps = {
+    
+};
+
+const Homepage:React.FC<HomepageProps> = () => {
+    
+    return (
+    <div className=''>
+        Have a good coding
+    </div>
+    );
+}
+export default Homepage;
--- java-website/src/app/page.tsx
+++ java-website/src/app/page.tsx
@@ -1,9 +1,25 @@
+import Homepage from '@/Components/Homepage';
 import Image from 'next/image'
 
 export default function Home() {
+  const backgroundImageUrl = '../Background/Background_3.jpg';
+
   return (
-    <div className=''>
-      Hello
+    <div className="relative h-screen bg-cover bg-center" >
+      
+      <div className="absolute z-0 inset-0 bg-cover bg-center blur-sm h-screen w-screen" style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
+        {/* Bg Image */}
+      </div> 
+      <div className="absolute z-1 inset-0 bg-overlay h-screen w-screen bg-black opacity-40">
+        {/* Overlay Black */}
+      </div> 
+
+
+      {/* Your content goes here */}
+      <div className='relative h-screen w-screen z-10'>
+        <Homepage />
+      </div>
+      
     </div>
-  )
+  );
 }
